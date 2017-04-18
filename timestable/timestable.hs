8efc60a07946cde6caa23511ed833d2da54d5cf2
James Ball <hello@james-ball.co.uk>
2017-04-18 14:29:19 +0100

    Added comments and Types

--- timestable/timestable.hs
+++ timestable/timestable.hs
@@ -1,9 +1,22 @@
+-- Where the timestable starts
+start :: Integer
 start = 1
+
+-- Where the timestable ends
+limit :: Integer
 limit = 12
 
+-- The range of the timestable
+range :: [Integer]
 range = [start..limit]
 
+-- This function will create a list based
+-- on the multiplier value and will
+-- multiply it for each item in the range
+times :: Integer -> [Integer]
 times multiplier = [x * multiplier | x <- range]
 
+-- Comprehends the timestables values into a table
+table :: [(Integer, [Integer])]
 table = [(x * start, times x) | x <- range]
 
